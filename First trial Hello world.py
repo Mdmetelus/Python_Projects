fba558af156293327b9206add0303204c545c904
Mr. Metelus <Mdmetelus@gmail.com>
2018-07-07 16:02:55 -0400

    Add files via upload

---
+++ First trial Hello world.py
@@ -0,0 +1,55 @@
+#1/usr/bin/python3
+
+# My first program
+
+
+
+'''Multyline quote
+is going'''
+
+"""print( "Hi")
+
+name =input ('Enter your Name: ')
+
+print("Hi", name )
+
+age = input('Enter your age: ')
+
+print(type(age))
+
+age = int( age)
+
+print(type(age))
+print(x)"""
+
+#1/usr/bin/python3
+
+#demeonstrate Integers
+
+"""x, y, z, = 1, 2, 3,"""
+
+
+
+'''def boolEx(a, b):
+    print(not b)
+    d= (1> 2)
+    print (d)
+
+
+boolEx(True, False)'''
+
+##import random
+##for i in range(15):
+##   randomnumber = random.randint(1,10)
+
+print( 3*4)
+print(3*'ab')
+print('a'+'bcd')
+
+str(3)+'ab'
+print9=5
+
+randint(1,100)
+print( random.randint(1,100))
+
+
