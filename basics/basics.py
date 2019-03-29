817053cefb833fc5abde6fc46e30b1b9ee15d9da
Mr. Metelus <Mdmetelus@gmail.com>
2019-03-29 16:34:02 -0400

    update

--- basics/basics.py
+++ basics/basics.py
@@ -7,4 +7,17 @@ pin = int(input("Enter your pin: "))
 def find_in_file(f):
     myfile = open("sample.txt")
     fruits = myfile.read()
-    fruits = fruits.split
+    fruits = fruits.splitlines()
+    if f in fruits:
+        return "That Fruit is in the List."
+    else:
+        return "No such fruit found!"
+    
+if pin in pins.values():
+    fruit = input("Enter fruit: ")
+    print(find_in_file(fruit))
+else:
+    print("Incorrect pin!")
+    print("This info can be accsessed only by: ")
+    for key in pins.keys():
+        print(key)
