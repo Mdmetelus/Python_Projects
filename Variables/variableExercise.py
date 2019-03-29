f4b1dcb93eb80b1d3256a5a4c764ba74fddde494
Mr. Metelus <Mdmetelus@gmail.com>
2019-03-29 17:59:03 -0400

    some variable exersizes completed

---
+++ Variables/variableExercise.py
@@ -0,0 +1,34 @@
+character = "Jon Snow"
+
+price = 10
+print(price)
+
+"""
+Bad examples:
+
+print = 10 
+
+int = 121 
+
+Good examples:
+
+printing = 10 
+
+print_variable = 10 
+
+int_var = 121 
+"""
+# Math Operations
+# 1 + 3.5
+# 4.5
+print(1 + 3.5)
+
+
+
+x = 1
+y = 2
+z = 3
+
+a = ((x * y)**z) / 8
+
+print(a)
