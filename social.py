6dde6fa4bb65a45e8166c76f6017a86c98e7db3d
Mr. Metelus <Mdmetelus@gmail.com>
2019-04-10 12:43:54 -0400

    added Queue, user and social classes

---
+++ social.py
@@ -0,0 +1,34 @@
+import random
+import math
+import time
+
+class Queue:
+  # bfs
+  def __init__(self):
+      self.queue =[]
+  def enqueue(self,value):
+      self.queue.append(value)
+  def dequeue(self)
+      if(self.size) > 0
+          return self.queu.pop(0)
+      else:
+          return None
+  def size(self):
+      return len(self.queue)
+
+class User:
+    def __init__(self, name):
+        self.name = name
+
+class SocialGraph:
+    def __init(self):
+        self.lastID = 0
+        self.users = {}
+        self.friendships = {}
+  
+    def addFriendship(self, userID, FriendID):
+        if userID('You cannot be you own friend")
+        elif friendID in self.friendships[userID] or userID in self.friendships[friendID]:
+            print("You already are friends with this person")
+        else:
+            self.friendships[userId].add(friendID)
